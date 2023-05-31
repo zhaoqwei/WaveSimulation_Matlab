31cebb1baa1a2ce644cd5b7eabfd7ae996fbaece
zhaoqingwei <zhaoqwei001@163.com>
2023-05-31 17:39:02 +0800

    cpml xyz

--- cpml.m
+++ cpml.m
@@ -18,7 +18,7 @@ function [ aaz,bbz,aax,bbx,aay,bby ] = cpml( R,Size,pmln,Vmax,dt,dz)
     
     if nD==2
         z=Size(1);x=Size(2);
-        ddz=zeros(z,x);
+        ddz=zeros(z,x);ddx=zeros(x,z);
         I=pmln;
         for iz=1:z
             if iz<=I
@@ -28,18 +28,31 @@ function [ aaz,bbz,aax,bbx,aay,bby ] = cpml( R,Size,pmln,Vmax,dt,dz)
                 ddz(iz,:)=(-z+I+iz)*(-z+I+iz)/I/I*ones(1,x);
             end
         end
+        for ix=1:x
+            if ix<=I
+                ddx(ix,:)=(I-ix+1)*(I-ix+1)/I/I*ones(1,z);
+            end
+            if ix>=x-I
+                ddx(ix,:)=(-x+I+ix)*(-x+I+ix)/I/I*ones(1,z);
+            end
+        end
+        
         AA=-log(R)*Vmax*3/2/(pmln*dz);
         ddz=ddz*AA;
         bbz=exp(-ddz*dt);
         aaz=bbz-1;
-        bbx = permute(bbz,[2 1 3]);
-        aax = permute(aaz,[2 1 3]);
+        ddx=ddx*AA;
+        bbx1=exp(-ddx*dt);
+        aax1=bbx1-1;
+        
+        bbx = permute(bbx1,[2 1 3]);
+        aax = permute(aax1,[2 1 3]);
         bby=[];
         aay=[];
     end
     if nD==3
         z=Size(1);x=Size(2);y=Size(3);
-        ddz=zeros(z,x,y);
+        ddz=zeros(z,x,y);ddx=zeros(x,z,y);ddy=zeros(y,x,z);
         I=pmln;
         for iz=1:z
             if iz<=I
@@ -49,11 +62,29 @@ function [ aaz,bbz,aax,bbx,aay,bby ] = cpml( R,Size,pmln,Vmax,dt,dz)
                 ddz(iz,:,:)=(-z+I+iz)*(-z+I+iz)/I/I*ones(x,y);
             end
         end
+        for ix=1:x
+            if ix<=I
+                ddx(ix,:,:)=(I-ix+1)*(I-ix+1)/I/I*ones(z,y);
+            end
+            if ix>=x-I
+                ddx(ix,:,:)=(-x+I+ix)*(-x+I+ix)/I/I*ones(z,y);
+            end
+        end
+        for iy=1:y
+            if iy<=I
+                ddy(iy,:,:)=(I-iy+1)*(I-iy+1)/I/I*ones(x,z);
+            end
+            if iy>=y-I
+                ddy(iy,:,:)=(-y+I+iy)*(-y+I+iy)/I/I*ones(x,z);
+            end
+        end
         AA=-log(R)*Vmax*3/2/(pmln*dz);
-        ddz=ddz*AA;
+        ddz=ddz*AA;ddx=ddx*AA;ddy=ddy*AA;
         bbz=exp(-ddz*dt);aaz=bbz-1;
-        bbx = permute(bbz,[2 1 3]);aax = permute(aaz,[2 1 3]);
-        bby = permute(bbz,[3 2 1]);aay = permute(aaz,[3 2 1]);
+        bbx1=exp(-ddx*dt);aax1=bbx1-1;
+        bby1=exp(-ddy*dt);aay1=bby1-1;
+        bbx = permute(bbx1,[2 1 3]);aax = permute(aax1,[2 1 3]);
+        bby = permute(bby1,[3 2 1]);aay = permute(aay1,[3 2 1]);
     end
 end
 
